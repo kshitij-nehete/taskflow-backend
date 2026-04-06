c3dc5bbc27a010af9af71b45ae5b6543ca3f75d6
kshitij-nehete <kshitijnehete@gmail.com>
2026-04-06 22:56:22 +0530

    mongodb repositories

---
+++ src/main/java/com/taskflow/api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.taskflow.api.repository;
+
+import com.taskflow.api.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+
+}
